b13a0b702d77d4c652a5848d685ddb901f027b47
giddyan001 <gideonafolayan7@gmail.com>
2026-07-08 20:00:47 +0100

    Completed scripts 1 through 8

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,5 @@
 # Create a native segwit address and get the public key from the address.
+ADDRESS=$(bitcoin-cli -regtest getnewaddress "" "bech32")
+
+# 2. Get the address info and extract the public key
+bitcoin-cli -regtest getaddressinfo "$ADDRESS" | jq -r '.pubkey'
--- submission/05.sh
+++ submission/05.sh
@@ -3,3 +3,8 @@
 # Amount of 20,000,000 satoshis to this address: 2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP 
 # Use the UTXOs from the transaction below
 # transaction="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+# 1. Decode the raw transaction to grab the TXID and format the UTXOs into a JSON inputs array
+INPUTS=$(bitcoin-cli -regtest decoderawtransaction "$transaction" | jq -c '.txid as $txid | [.vout[] | {txid: $txid, vout: .n}]')
+
+# 2. Create the PSBT sending 0.2 BTC (20,000,000 satoshis) to the required address
+bitcoin-cli -regtest createpsbt "$INPUTS" '[{"2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP": 0.2}]'
